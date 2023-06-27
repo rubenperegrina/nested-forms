f87379b395037da040fd9a57c1430e8c31a1a9f9
Rubén Peregrina Aroca <ruben.peregrina@in2.es>
2023-06-27 13:50:29 +0200

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RatingNameComponent } from './components/rating-name/rating-name.component';
+import { StarRatingComponent } from './components/star-rating/star-rating.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, RatingNameComponent, StarRatingComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'nested-forms';
+	formGroup = new FormGroup({
+		title: new FormControl('El señor de los anillos', Validators.required),
+		rating: new FormControl(4, Validators.min(3)),
+		ratingName: new FormControl({ name: 'Juan', rating: 5 })
+	});
+
+	clickSave(): void {
+		console.log('valid--> ', this.formGroup.valid);
+		console.log('valid--> ', this.formGroup.controls.ratingName.errors);
+	}
 }
